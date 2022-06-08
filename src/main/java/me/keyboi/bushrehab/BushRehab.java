63e2ee1e2dde95722c970a9261183ff7f46858d0
KeybladeCrafter <31298338+KeybladeCrafter@users.noreply.github.com>
2022-06-08 12:27:27 -0400

    Update BushRehab.java

--- src/main/java/me/keyboi/bushrehab/BushRehab.java
+++ src/main/java/me/keyboi/bushrehab/BushRehab.java
@@ -18,50 +18,20 @@ import org.bukkit.persistence.PersistentDataContainer;
 import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.potion.PotionData;
 import org.bukkit.potion.PotionType;
 
 
 public final class BushRehab extends JavaPlugin implements Listener {
-    private NamespacedKey bushStateKey;
+   // private NamespacedKey bushStateKey;
 
     @Override
     public void onEnable() {
-        Bukkit.getConsoleSender().sendMessage(ChatColor.GREEN + "BushRehab opening up shop");
+        Bukkit.getConsoleSender().sendMessage("_/\\_ BushRehab opening up shop _/\\_");
         getServer().getPluginManager().registerEvents(new PlayerUseWaterPotionListener(), this);
-        bushStateKey = new NamespacedKey(this, "bushstate");
-
-        // getServer().getPluginManager().registerEvents(new PlayerUseBonemealListener(), this);
-        //  getServer().getPluginManager().registerEvents(new BushGrowingListener(), this);
-    }
-    @EventHandler
-    public void changeBushState(PlayerInteractEvent event) {
-        final Block block = event.getClickedBlock();
-        assert block != null;
-        Player player = event.getPlayer();
-        PotionMeta meta = (PotionMeta) player.getInventory().getItemInMainHand().getItemMeta();
-        PotionType potiontype = meta.getBasePotionData().getType();
-
-        final PersistentDataContainer customBlockData = new CustomBlockData(block, this);
-        
-        if (event.getAction() == Action.RIGHT_CLICK_BLOCK) {
-            player.sendMessage("right clicked");
-            
-            if (block.getType() == Material.POTTED_DEAD_BUSH && potiontype == PotionType.WATER) {
-                player.sendMessage("clicking bush with water");
-                
-                if (customBlockData.isEmpty()) {
-                    customBlockData.set(bushStateKey, PersistentDataType.INTEGER, 1);
-                    player.sendMessage("bushstate is empty");
-                }
-                else if (!customBlockData.isEmpty()){
-                    player.sendMessage("bushstate isnt empty");
-                }
-            }
-        }
-
-    }
+ 
         @Override
         public void onDisable () {
-            Bukkit.getConsoleSender().sendMessage(ChatColor.GREEN + "BushRehab closing up shop");
+            Bukkit.getConsoleSender().sendMessage("_/\\_ BushRehab closing up shop _/\\_");
         }
 }

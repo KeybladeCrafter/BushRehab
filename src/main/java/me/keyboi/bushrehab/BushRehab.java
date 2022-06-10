ddd30b5d78c4a1c1dc0ffa0713214be4896ab0b9
BuildTools <unconfigured@null.spigotmc.org>
2022-06-09 23:41:51 -0400

    cleanup

--- src/main/java/me/keyboi/bushrehab/BushRehab.java
+++ src/main/java/me/keyboi/bushrehab/BushRehab.java
@@ -1,23 +1,12 @@
 package me.keyboi.bushrehab;
 
+import me.keyboi.bushrehab.listener.PlayerUseBonemealListener;
 import me.keyboi.bushrehab.listener.PlayerUseWaterPotionListener;
+import me.keyboi.bushrehab.tasks.ChangeBushData;
 import org.bukkit.*;
-import org.bukkit.block.Block;
-import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.Action;
-import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.inventory.meta.PotionMeta;
-import org.bukkit.persistence.PersistentDataContainer;
-import org.bukkit.persistence.PersistentDataType;
-import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.potion.PotionData;
-import org.bukkit.potion.PotionType;
-import org.bukkit.scheduler.BukkitRunnable;
-import org.bukkit.scheduler.BukkitScheduler;
 import org.bukkit.scheduler.BukkitTask;
 
 
@@ -45,8 +34,7 @@ public final class BushRehab extends JavaPlugin implements Listener {
 
     private void registerEvents() {
         PluginManager pm = getServer().getPluginManager();
-
+        pm.registerEvents(new PlayerUseBonemealListener(this), this);
         pm.registerEvents(new PlayerUseWaterPotionListener(this), this);
-
     }
 }
